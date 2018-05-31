f041fb0535718759b68582bb8842d80fce72f8e6
mfarismuzakki <mfarismuzakki@gmail.com>
2018-05-31 13:40:23 +0700

    aaaa

--- CI/application/controllers/LoginController.php
+++ CI/application/controllers/LoginController.php
@@ -3,20 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class LoginController extends CI_Controller {
 
-	//konstruktor
-	function __construct(){
-		parent::__construct();
-	}
-
-
-	var $status = false;
 	
 	public function index()
 	{
+<<<<<<< HEAD
 		$status['status'] = $this->status;
 		$this->load->view('loader/header');
 		$this->load->view('loginview',$status);
 		$this->load->view('loader/footer');
+=======
+		$this->load->view('loadbootstrap');
+		$this->load->view('loginview');
+>>>>>>> parent of 9dd83ae... update tampilan login(+session), home user
 	}
 
 
@@ -25,28 +23,7 @@ class LoginController extends CI_Controller {
 		$data['username'] = $this->input->post('username');
 		$data['password'] = $this->input->post('password');
 
-		//load model
-		$this->load->model('UsersModel');
-
-
-		if($this->UsersModel->getData($data)->num_rows()){
-			
-			$userdata = array(
-				'username' => $data['username'],
-				'password' => $data['password'],
-				'logod_in' => TRUE
-			);
-			$this->session->userdata($userdata);
-
-			redirect('HomeUserController');
-
-		}else{
-			$this->status = true;
-			$this->index();
-		}
 
 	}
-
-	
 }
 ?>
