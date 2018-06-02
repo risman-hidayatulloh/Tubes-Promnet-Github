f3f05317556232b3c856b8553f087f34d2f05669
risman100 <risman100@student.upi.edu>
2018-06-02 16:12:27 +0700

    pdf

--- CI/application/controllers/HomeUserController.php
+++ CI/application/controllers/HomeUserController.php
@@ -98,12 +98,24 @@
 	        $pdf->Cell(190,5,'Jln. Dr. Setiabudi No. 157, Bandung',0,1,'C');
 	        $pdf->Cell(10,7,'',0,1);
 
-	        $pdf->Cell(100,7,'No. Faktur : ',0,0);
-	        $pdf->Cell(0,7,'Nama : ',0,1);
-	        $pdf->Cell(100,7,'Tanggal : ',0,0);
-	        $pdf->Cell(0,7,'Type/No. Pol : ',0,1);
-	        $pdf->Cell(100,7,'No. Kartu : ',0,0);
-	        $pdf->Cell(0,7,'Keterangan : ',0,1);
+	        $query2 = $this->db->query('SELECT * FROM pelanggans');
+			$row = $query2->row();
+			//echo $row->kode_pelanggan;
+			//$query2->free_result();
+
+	        $pdf->Cell(25,7,'Kode',0,0);
+	        $pdf->Cell(75,7,": $row->kode_pelanggan",0,0);
+	        $pdf->Cell(25,7,'Nama',0,0);
+	        $pdf->Cell(75,7,": $row->nama_pelanggan",0,1);
+
+	        $pdf->Cell(25,7,'Alamat',0,0);
+	        $pdf->Cell(75,7,": $row->alamat_pelanggan",0,0);
+	        $pdf->Cell(25,7,'Plat Nomor',0,0);
+	        $pdf->Cell(75,7,": $row->plat_nomor",0,1);
+
+	        $pdf->Cell(25,7,'No STNK',0,0);
+	        $pdf->Cell(75,7,": $row->no_stnk",0,1);
+	        
 	        $pdf->Cell(10,7,'',0,1);
 
 	        $pdf->Cell(100,7,'Rincian Pembelian',0,0);
